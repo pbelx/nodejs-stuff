43f7c65a6a2dadb71b0eb1e457311c70a5385145
Peter B <31408676+pbelx@users.noreply.github.com>
2021-04-12 00:33:13 +0300

    Create stations.js
    radio station list

---
+++ online-radio/stations.js
@@ -0,0 +1,12 @@
+var radio = {
+    test:"http://127.0.0.1:8000/pp.mp3",
+    kfm:"http://193.108.252.253:8000/xfm?",
+    radiocity:"http://162.244.80.106:10853/stream?",
+    sanyu:"http://s44.myradiostream.com:8138",
+    capital:"http://144.217.203.226:8316"
+
+}
+
+module.exports = {
+    radio
+}
